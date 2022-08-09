ddda390dd795c15eae285bfdd190aff6ad57970e
EmmyMay <lekeemmy@gmail.com>
2022-08-09 01:04:49 +0100

    installing windi

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'virtual:windi.css'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-
-const x = (r: any) => r
