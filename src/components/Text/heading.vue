48f7f961994cb155c8b0750a61b65ee7e9519a6d
EmmyMay <lekeemmy@gmail.com>
2022-08-17 16:08:53 +0100

    initial commit

---
+++ src/components/Text/heading.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+interface Props {
+  color?: string
+  tag?: string
+}
+</script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = withDefaults(defineProps<Props>(), {
+  color: 'black',
+  tag: 'h1',
+})
+const color = ref(`text-${props.color}`)
+</script>
+
+<template>
+  <component :is="tag" :class="[color, tag === 'h1' ? '' : '']"></component>
+</template>
+
+<style scoped></style>
