fbb5df89cfbc5c868cbd45eb38bfd6ce8ea7eba5
EmmyMay <lekeemmy@gmail.com>
2022-08-31 16:41:26 +0100

    first iteration of the icons

---
+++ src/components/icons/epl.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+interface Props {
+  width?: string
+  height?: string
+  fill?: string
+  color?: string
+}
+</script>
+<script setup lang="ts">
+withDefaults(defineProps<Props>(), {
+  width: '27',
+  height: '27',
+  fill: 'none',
+  color: '#797979',
+})
+</script>
+<template>
+  <img class="block" src="@/assets/epl.png" alt="The premier league logo" />
+</template>
